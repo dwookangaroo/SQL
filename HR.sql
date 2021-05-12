a358789e5eeb5cd25508db777664b3accb7532eb
강동우 <dwk9181@yahoo.com>
2021-05-12 18:10:41 +0900

    first commit

---
+++ HR.sql
@@ -0,0 +1,124 @@
+-- HR 계정
+-- 계정 내의 테이블 확인
+-- SQL은 대소문자 구분하지 않음
+SELECT * FROM tab; 
+-- 테이블의 구조 확인
+DESC employees;
+
+--------
+--SELECT ~ FROM
+--------
+
+--가장 기본적인 SELECT : 전체 데이터 조회
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+-- 테이블 내에 정의된 컬럼의 순서대로 출력됨
+-- 특정 컬럼만 선별적으로 projectionㄴ
+--모든 사원의 first_name, 입사일, 급여 출력해보자
+SELECT first_name, hire_date, salary
+FROM employees;
+
+--기본적 산술연산을 수행
+-- 산술식 자체가 틀정 테이블에 소속된 것이 아닐때 "dual"
+SELECT 10 + 20 FROM dual;
+-- 특정 컬럼 값을 수치로 산술계산을 할수있다.
+-- 직원들의 연봉 salary * 12
+SELECT first_name,
+    salary,
+    salary * 12
+from employees;
+
+-- 
+select first_name, job_id * 12 from employees; --error
+desc employees; -- job_id는 문자열이기때문에 산술연산 불가
+
+--연습
+--employees 테이블, first_name, phone_number, 
+-- hire_date, salary를 출력
+select first_name,
+    phone_number,
+    hire_date,
+    salary
+from employees;
+
+--사원의 first_name, last_name, salary, 
+-- phone_number, hire_ date
+-- 직접해보기
+select first_name, last_name, salary,
+    phone_number, hire_date
+from employees;
+
+-- 문자열의 연결 ||
+-- first_name last_name을 연결 출력
+select first_name || ' ' || last_name
+from employees;
+
+select first_name, salary, commission_pct
+from employees;
+
+-- 커미션 포함, 실질 급여를 출력해 봅시다
+select 
+    first_name,
+    salary,
+    commission_pct,
+    salary + salary * commission_pct
+from employees;
+    
+-- 산술 연산식에 null이 포함되어 있으면 결과 항상 null
+
+-- nvl(expr1, expr2) : expr1이 null이면 expr2 선택
+select
+    first_name,
+    salary,
+    commission_pct,
+    salary + salary * nvl(commission_pct, 0)
+from employees;
+
+-- Alias (별칭)
+select
+    first_name 이름,
+    last_name as 성,
+    first_name || ' ' || last_name "Full Name" 
+    -- 별칭내에 공백, 특수문자가 포함될 경우 "로 묶는다
+from employees;
+
+-- 필드 표시명은 일반적으로 한글 등은 쓰지 말자
+
+----------------
+--where
+----------------
+-- 조건을 기준으로 레코드를 선택(selection)한다
+
+-- 급여가 15000이상인 사원의 이름과 연봉
+select first_name, salary * 12 "Annual Salary"
+from employees
+where salary >= 15000;
+
+-- 07/01/01 이후 입사한 사원의 이름과 입사일
+select first_name, hire_date
+from employees
+where hire_date >= '07/01/01';
+
+-- 이름이 Lex인 사원의 연봉, 입사일, 부서 id
+select first_name, salary * 12 "Annual Salary",
+    hire_date, department_id
+from employees
+where first_name = 'Lex';
+
+-- 부서 id가 10인 사원의 명단
+select * from employees
+where department_id = 10;
+
+--alias 연습
+select first_name || ' ' || last_name 이름,
+    hire_date 입사일, phone_number 전화번호,
+    salary 급여, salary * 12 연봉
+    from employees;
+    
+-- 논리조합
+-- 급여가 14000이하 or 17000이상인 사원의 이름과 급여
+
+
+
+
